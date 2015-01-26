32eb2418418c02f0efb623046c12c0cfb18ca257
jeremy simmons <jsimmons@jeremysimmons.net>
2015-01-26 12:17:26 -0600

    feat (tfs2012objectmodel): add package for tfs2012 object model

---
+++ tfs2012objectmodel/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'tfs2012objectmodel'
+$installerType = 'exe'
+$url = 'https://visualstudiogallery.msdn.microsoft.com/f30e5cc7-036e-449c-a541-d522299445aa/file/89950/2/tfs_ObjectModel.exe'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
