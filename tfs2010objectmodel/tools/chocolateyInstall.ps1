920fe25fdce2b8cc07ca4f18fa280d025abbdd1c
jeremy simmons <jsimmons@jeremysimmons.net>
2015-01-26 12:40:08 -0600

    feat (tfs2010objectmodel): add package for tfs2010 object model

---
+++ tfs2010objectmodel/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'tfs2010objectmodel'
+$installerType = 'exe'
+$url = 'https://visualstudiogallery.msdn.microsoft.com/a37e19fb-3052-4fc9-bef7-4a4682069a75/file/76413/1/TFS_ObjectModel_enu.exe'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
