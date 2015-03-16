98cc5e78f75e6e277637d74735fbd735ec6fd413
jeremy simmons <jsimmons@jeremysimmons.net>
2015-03-16 11:40:04 -0600

    feat (tfs2013objectmodel) update to version 12.0.31101.0 - 2013 Update 4

--- tfs2013objectmodel/tools/chocolateyInstall.ps1
+++ tfs2013objectmodel/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'tfs2013objectmodel'
 $installerType = 'exe'
-$url = 'https://visualstudiogallery.msdn.microsoft.com/3278bfa7-64a7-4a75-b0da-ec4ccb8d21b6/file/116382/1/tfs_ObjectModel.exe'
+$url = 'https://visualstudiogallery.msdn.microsoft.com/19311823-5262-4e63-a586-2283384ae3bf/file/146393/1/tfs_ObjectModel.exe'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
